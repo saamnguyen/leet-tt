8968cfd7ba42bce8df19ba9dcc9b59d253da1bf8
finn-01 <nguyensam576@gmail.com>
2022-05-26 14:06:43 +0700

    Valid Parentheses

--- src/easy/ValidParentheses.java
+++ src/easy/ValidParentheses.java
@@ -10,30 +10,33 @@ public class ValidParentheses {
         //check
         if(s == null || s.length() == 0) return false;
 
-        //System.out.println(s.toCharArray());
+        System.out.println(s.toCharArray());
 
         //loop
         for(char c: s.toCharArray()){
-          //  System.out.println(c);
-           // System.out.println(stack + " stack start");
+            System.out.println(c);
+            System.out.println(stack + " stack start");
             if(c == '('){
                 stack.push(')');
-            //    System.out.println(0);
+                System.out.println(0);
+
             }
             else if (c == '{'){
 
                 stack.push('}');
-            //    System.out.println(1);
+                System.out.println(1);
             }
             else if (c == '['){
                 stack.push(']');
-              //  System.out.println(2);
+                System.out.println(2);
             }
             else if(stack.isEmpty() || stack.pop() != c){
-            //    System.out.println("empty");
+                System.out.println("123");
                 return  false;
+
             }
-          //  System.out.println(stack + " stack end");
+            System.out.println(stack + " stack end");
+            System.out.println(stack.isEmpty());
         }
         return stack.isEmpty();
     }
@@ -41,7 +44,7 @@ public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses validParentheses = new ValidParentheses();
 
-        System.out.println(validParentheses.isValid("{[]()}"));
+        System.out.println(validParentheses.isValid("()()"));
     }
 }
 
