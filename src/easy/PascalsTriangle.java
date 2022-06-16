0603abb6dd64ba2b8004862a2af364dbdad19ccd
finn-01 <nguyensam576@gmail.com>
2022-06-17 02:06:06 +0700

    Valid Palindrome

---
+++ src/easy/PascalsTriangle.java
@@ -0,0 +1,14 @@
+package easy;
+
+import java.util.List;
+
+public class PascalsTriangle {
+//    public List<List<Integer>> generate(int numRows){
+//
+//    }
+}
+
+//Input: numRows = 5
+//        Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//        Input: numRows = 1
+//        Output: [[1]]
